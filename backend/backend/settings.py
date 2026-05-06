ba78d2db2ed52bbcc63aab927a09ac38a4efc423
Ali Huseyinoglu <lackfadiriso2005@gmail.com>
2026-05-06 22:02:21 +0300

    add setup config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,4 @@
 import os
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,9 +9,6 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 DEBUG = True
 
 ALLOWED_HOSTS = ['158.160.244.222', 'localhost', '192.168.1.102', '127.0.0.1']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
