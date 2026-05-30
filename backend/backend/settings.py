1078d5cf597c63f8512f884d7f3e6c571d72888a
Ali Hüseyinoğlu <62191640+lackfadiriso@users.noreply.github.com>
2026-05-30 04:51:05 +0300

    added auto restart

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.244.222', 'localhost', '192.168.1.102', '127.0.0.1']
+ALLOWED_HOSTS = ['158.160.244.222', 'localhost', 'taskis.ddns.net', '127.0.0.1']
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
